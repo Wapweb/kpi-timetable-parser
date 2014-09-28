f55634034da5d3ede1bb70138bf952c69268d178
Wapweb <abit-poisk@yandex.ua>
2014-09-28 23:55:12 +0300

    Update db_conn.php

--- inc/db_conn.php
+++ inc/db_conn.php
@@ -1,4 +1,4 @@
 <?php
-$db = new PDO("mysql:dbname=rozklad;host=localhost","wapweb","1111");
+$db = new PDO("mysql:dbname=DATABASE_NAME;host=HOST_ADDRESS","USER_NAME","USER_PASSWORD");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->query("SET NAMES utf8");
